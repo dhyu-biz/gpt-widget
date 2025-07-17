6ce6aa0b54624653bf4eb53d59e9555dc8973cfa
YUDAHAN <dbekgks98@gmail.com>
2025-07-17 19:23:45 +0900

    init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/main.jsx',
+      name: 'MyWidget',
+      fileName: 'my-widget',
+      formats: ['iife'], // 하나의 JS 파일로 빌드
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+})
